8c398ee3a53bb4732c6f0805770c8d753b222c5b
20alan01 <20alan01@gmail.com>
2024-05-13 14:44:05 +0200

    Add project files.

---
+++ DataGeneratorCsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DatageneratorCsharp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=DESKTOP-QUHRVG6\\MSSQLServer02;Database=APPS;Trusted_Connection=True;TrustServerCertificate=true;";
+                conn.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM dbo.Medication", conn);
+                // using the code here...
+               using (SqlDataReader reader = command.ExecuteReader()) 
+                { 
+                    while (reader.Read()) 
+                    {
+                        Console.WriteLine(String.Format("{0} \t | {1} \t | {2}",
+                         // call the objects from their index
+                         reader[0], reader[1], reader[2]));
+                    }
+                }
+               
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
