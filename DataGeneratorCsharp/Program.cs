b652c61b4bf396000bb4e525c5b32a42cef94f4e
20alan01 <20alan01@gmail.com>
2024-05-14 10:21:15 +0200

    Add documentation

--- DataGeneratorCsharp/Program.cs
+++ DataGeneratorCsharp/Program.cs
@@ -10,7 +10,8 @@ namespace DatageneratorCsharp
 {
     internal class Program
     {
-        public class HeartRateData
+       
+        public class HeartRateData // use heartratedata class to make forwarding data to the database more consistent
         {
             public int SensorId;
             public int HeartRateBPM;
@@ -27,20 +28,20 @@ namespace DatageneratorCsharp
 
              static void GetExcelData(int i, int j)
             {
-                string HeartRateSet1Path = "C:\\Users\\Alan Osefia\\Desktop\\DataGeneratorC#\\DataGeneratorCsharp\\DataGeneratorCsharp\\HeartRateSet1.xlsx";
+                
+                string HeartRateSet1Path = "C:\\Users\\Alan Osefia\\Desktop\\DataGeneratorC#\\DataGeneratorCsharp\\DataGeneratorCsharp\\HeartRateSet1.xlsx"; // This is excel path for my PC.
                 _Application excel = new _Excel.Application();
-                Workbook wb = excel.Workbooks.Open(HeartRateSet1Path);
-                Worksheet ws = wb.Worksheets[1];
+                Workbook wb = excel.Workbooks.Open(HeartRateSet1Path); // Workbook.Open opens the excel sheets
+                Worksheet ws = wb.Worksheets[1];// Worksheets selects which worksheet you want to use
 
-     
+
                 int numRows = 122-1;
                 int numColumns = 4; 
                 
-                for (int x = 2; i <= numRows; x++)
+                for (int x = 2; i <= numRows; x++) // For loop to put all the excel values into the class
                 {
                     HeartRateData data = new HeartRateData();
 
-   
                     var test = ws.Cells[x, 1].Value2;
                     data.SensorId = Convert.ToInt32(ws.Cells[x, 1].Value2);
                     data.HeartRateBPM = Convert.ToInt32(ws.Cells[x, 2].Value2);
@@ -49,8 +50,9 @@ namespace DatageneratorCsharp
                     data.Condition = Convert.ToString(ws.Cells[x, 5].Value2);
 
                     InsertHeartRateToDb(data);
-                    Thread.Sleep(60000);
-                   
+                    
+                    Thread.Sleep(60000); // 1 minute timer
+
                     i++;
                 }
 
@@ -60,13 +62,13 @@ namespace DatageneratorCsharp
             {
                 using (SqlConnection conn  = new SqlConnection())
                 {
-                    conn.ConnectionString = "Server=DESKTOP-QUHRVG6\\MSSQLServer02;Database=APPS;Trusted_Connection=True;TrustServerCertificate=true;";
-                    conn.Open();
+                    conn.ConnectionString = "Server=DESKTOP-QUHRVG6\\MSSQLServer02;Database=APPS;Trusted_Connection=True;TrustServerCertificate=true;"; 
+                    conn.Open(); //Opens the connection with the Db
                     SqlCommand insertCommand = new SqlCommand("INSERT INTO dbo.HeartRateData (SensorId,HeartRateBPM,EnterTime) VALUES (@0, @1,@2)", conn);
                     insertCommand.Parameters.Add(new SqlParameter("0", heartRate.SensorId));
                     insertCommand.Parameters.Add(new SqlParameter("1", heartRate.HeartRateBPM));
                     insertCommand.Parameters.Add(new SqlParameter("2", heartRate.TimeStamp));
-                    Console.WriteLine("Commands executed! Total rows affected are " + insertCommand.ExecuteNonQuery() + $"; sensorId={heartRate.SensorId} heartrate={heartRate.HeartRateBPM}, EnterTime={heartRate.TimeStamp}");
+                    Console.WriteLine("Commands executed! Total rows affected are " + insertCommand.ExecuteNonQuery() + $"; sensorId={heartRate.SensorId} heartrate={heartRate.HeartRateBPM}, EnterTime={heartRate.TimeStamp}"); // Adds the data into the db, ands shows the enterd data in the console
                 
                 }
             }
