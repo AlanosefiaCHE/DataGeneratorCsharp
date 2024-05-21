32b08cee4d2de638e7e40d47fcd1ce3c13ec9eed
Alan Osefia <amosefia@student.che.nl>
2024-05-21 12:17:28 +0200

    Made incorrect dummy data that can be added to db.

--- DataGeneratorCsharp/Program.cs
+++ DataGeneratorCsharp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
 using Microsoft.Office.Interop.Excel;
 using _Excel = Microsoft.Office.Interop.Excel;
 
@@ -8,15 +10,15 @@ namespace DatageneratorCsharp
     {
         public class HeartRateData // use heartratedata class to make forwarding data to the database more consistent
         {
-            public int SensorId;
-            public int HeartRateBPM;
-            public DateTime TimeStamp;
+            public int? SensorId;
+            public int? HeartRateBPM;
+            public DateTime? TimeStamp;
 
-            public HeartRateData(int sensorId, int heartRateBPM)
+            public HeartRateData(int sensorId, int heartRateBPM, DateTime? timeStamp)
             {
                 SensorId = sensorId;
                 HeartRateBPM = heartRateBPM;
-                TimeStamp = DateTime.Now;
+                TimeStamp = timeStamp;
             }
         }
         static void Main(string[] args)
@@ -43,32 +45,63 @@ namespace DatageneratorCsharp
             _Application excel = new _Excel.Application();
             Workbook wb = excel.Workbooks.Open(HeartRateSet1Path); // Workbook.Open opens the excel sheets
             Worksheet ws = wb.Worksheets[1];// Worksheets selects which worksheet you want to use
-            //Worksheet ws2 = wb.Worksheets[2];
+            Worksheet ws2 = wb.Worksheets[2];
+            Worksheet ws3 = wb.Worksheets[3];
 
             for (int currentRow = startRow; currentRow < numRows; currentRow++) // For loop to put all the excel values into the class
             {
                 HeartRateData heartRateDataRow = new HeartRateData(
                     sensorId: Convert.ToInt32(ws.Cells[currentRow, sensorIdIndex].Value2),
-                    heartRateBPM: Convert.ToInt32(ws.Cells[currentRow, heartRateIndex].Value2)
+                    heartRateBPM: Convert.ToInt32(ws.Cells[currentRow, heartRateIndex].Value2),
+                    timeStamp: DateTime.Now
                 );
                 //Second sensor
-                //HeartRateData heartRateDataRow2 = new HeartRateData(
-                //    sensorId: Convert.ToInt32(ws2.Cells[currentRow, sensorIdIndex].Value2),
-                //    heartRateBPM: Convert.ToInt32(ws2.Cells[currentRow, heartRateIndex].Value2)
-                //);
+                HeartRateData heartRateDataRow2 = new HeartRateData(
+                    sensorId: Convert.ToInt32(ws2.Cells[currentRow, sensorIdIndex].Value2),
+                    heartRateBPM: Convert.ToInt32(ws2.Cells[currentRow, heartRateIndex].Value2),
+                    timeStamp: DateTime.Now
+                );
+                
+                HeartRateData heartRateDataRow3 = new HeartRateData(
+                    sensorId: ConvertStringToIntOrNull(ws3.Cells[currentRow, sensorIdIndex].Value2),
+                    heartRateBPM: ConvertStringToIntOrNull(ws3.Cells[currentRow, heartRateIndex].Value2),
+                    timeStamp: RandomDateTimeOrNull()
+
+                );
                 InsertHeartRateToDb(conn,  heartRateDataRow);
-               // InsertHeartRateToDb(conn,  heartRateDataRow2);
+                InsertHeartRateToDb(conn,  heartRateDataRow2);
+                InsertHeartRateToDb(conn,  heartRateDataRow3);
 
                 Thread.Sleep(2000);
             }
         }
+        static int? ConvertStringToIntOrNull(dynamic? InputValue)
+        {
+            try
+            {
+                return Convert.ToInt32(InputValue);
+            }
+            catch (Exception e)
+            {
+                return null;
+        
+            }
+        }
+
+        static DateTime? RandomDateTimeOrNull()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(4);
+            return  randomNumber == 0 ? null : DateTime.Now;
+        }
+
         static void InsertHeartRateToDb(SqlConnection conn, HeartRateData heartRate)
         {
             //Opens the connection with the Db
             SqlCommand insertCommand = new SqlCommand("INSERT INTO dbo.HeartRateData (SensorId,HeartRateBPM,EnterTime) VALUES (@SensorId, @HeartRateBPM,@TimeStamp)", conn);
             insertCommand.Parameters.Add(new SqlParameter("SensorId", heartRate.SensorId));
             insertCommand.Parameters.Add(new SqlParameter("HeartRateBPM", heartRate.HeartRateBPM));
-            insertCommand.Parameters.Add(new SqlParameter("TimeStamp", heartRate.TimeStamp));
+            insertCommand.Parameters.Add(new SqlParameter("TimeStamp", heartRate.TimeStamp.HasValue ? heartRate.TimeStamp.Value : DBNull.Value ));
             Console.WriteLine("Commands executed! Total rows affected are " + insertCommand.ExecuteNonQuery() + $"; sensorId={heartRate.SensorId} heartrate={heartRate.HeartRateBPM}, EnterTime={heartRate.TimeStamp}"); // Adds the data into the db, ands shows the enterd data in the console
         }
     }
